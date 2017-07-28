7792a4ab0890e647c71a2508c65715b55999a4ef
Jessica Aiskel Aristimuño <jeakel@gmail.com>
2017-07-28 10:04:11 +0200

    Create basic structure
    Start project

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    watch = require('gulp-watch'),
+    postcss = require('gulp-postcss'),
+    autoprefixer = require('gulp-autoprefixer'),
+    browserSync = require('browser-sync').create(),
+    cssnano = require('gulp-cssnano'),
+    sourcemaps = require('gulp-sourcemaps'),
+    connect = require('gulp-connect');
+
+gulp.task('default', ['watch','connect']);
+
+gulp.task('sass', function() {
+  return gulp.src('app/scss/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(autoprefixer())
+    .pipe(cssnano())
+    .pipe(sourcemaps.write('maps'))
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+gulp.task('css', function () {
+    return gulp.src('app/**/*.css')
+        .pipe( postcss([ require('precss'), require('autoprefixer') ]) )
+        .pipe( gulp.dest('dist/') );
+});
+
+gulp.task('watch', ['browserSync', 'sass'], function (){
+  gulp.watch('app/scss/**/*.scss', ['sass']);
+  gulp.watch('app/*.html', browserSync.reload);
+});
+
+// browserSync
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  })
+})
+// gulp conect
+gulp.task('connect', function () {
+    connect.server({
+        root: 'dist',
+        port: 8001,
+        livereload: true
+    });
+});
